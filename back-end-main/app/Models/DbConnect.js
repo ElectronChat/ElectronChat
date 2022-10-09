b3aff214780b7d99afaf2af0c80afb99d72deadc
Zack Khalidov <46911135+zkhalido@users.noreply.github.com>
2022-10-09 15:33:39 -0600

    exporting DbConnector

--- back-end-main/app/Models/DbConnect.js
+++ back-end-main/app/Models/DbConnect.js
@@ -8,34 +8,32 @@ class DbConnector{
     {
         return this.port;
     }
-    
+
     getIp()
     {
         return this.IP;
     }
-    
+
     getAllMsgs(roomID)
     {
         // sql stuff
         return null;
     }
-    
+
     getMsg(msgID)
     {
         return null;
     }
-    
+
     sendMsg(msg)
     {
         return null;
     }
-    
+
     deleteMsg(msg)
     {
         return null;
     }
-    
-    
-    
-    
 }
+// exports class DbConnector to be used by other .js files 
+module.exports = DbConnector;
