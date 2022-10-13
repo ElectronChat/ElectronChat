4b45fdcde93da47eb102e1578c0789987f9d97e3
Philip Marshall <philipmarshall@Philips-MacBook-Pro.local>
2022-10-12 19:04:04 -0600

    ready for pull request

--- front-end-main/src/test/app/rooms/rooms.service.spec.ts
+++ front-end-main/src/test/app/rooms/rooms.service.spec.ts
@@ -1,5 +1,5 @@
-import { RoomsService } from "./rooms.service";
-import { RoomCreateJoin } from "./room-create-join.model";
+import { RoomsService } from "../../../app/rooms/rooms.service";
+import { RoomCreateJoin } from "../../../app/rooms/room-create-join.model";
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -19,7 +19,7 @@ describe('RoomsService', () => {
     comp.createRoom("ABC");
     expect(comp.getRooms()[0].roomCode).toEqual("ABC");
     comp.createRoom("DEF");
-    expect(comp.getRooms()[0].roomCode).toEqual("DEF");
+    expect(comp.getRooms()[1].roomCode).toEqual("DEF");
 
   });
 
