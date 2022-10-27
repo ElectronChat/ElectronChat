eeaf8dec999f4c3dd3ebbfcd80b21f90be82d415
zkhalido <zkhalido@msudenver.edu>
2022-10-27 14:38:21 -0600

    adding broserRefresh

--- front-end-main/src/app/app.component.ts
+++ front-end-main/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router'
 import { Subscription } from 'rxjs';
+
+export let browserRefresh = false;
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,11 @@ import { Subscription } from 'rxjs';
 
 export class AppComponent {
   title = "ElectronChat";
+
+  constructor(private router: Router) {
+  this.subscription = router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        browserRefresh = !router.navigated;
+      }
+  });
 }
