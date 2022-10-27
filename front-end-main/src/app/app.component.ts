521acb28b54a500c95f2ab55b63a96b3a0108442
zkhalido <zkhalido@msudenver.edu>
2022-10-27 12:59:13 -0600

    working commit

--- front-end-main/src/app/app.component.ts
+++ front-end-main/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router'
 import { Subscription } from 'rxjs';
 
-export let browserRefresh = false;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,13 +10,4 @@ export let browserRefresh = false;
 
 export class AppComponent {
   title = "ElectronChat";
-  subscription: Subscription;
-
-  constructor(private router: Router) {
-    this.subscription = router.events.subscribe((event) => {
-        if (event instanceof onanimationstart) {
-          browserRefresh = !router.navigated;
-        }
-    });
   }
-}
--- front-end-main/src/app/rooms/room/room.component.ts
+++ front-end-main/src/app/rooms/room/room.component.ts
@@ -6,7 +6,6 @@ import { ActivatedRoute } from "@angular/router";
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { MessageComponent } from './message/message.component';
 import { NavigationStart, Router } from '@angular/router'
-import { browserRefresh } from '.../app/app.component';
 
 
 @Component ( {
@@ -32,9 +31,6 @@ export class RoomComponent implements OnInit, OnDestroy {
     this.route.params.subscribe(params=>{
       this.roomCode = params['id'];
   })
-
-  let browserRefresh = browserRefresh;
-  console.log('refreshed?:', BrowserRefresh);
 
     this.roomsService.setRoom(this.roomCode);
     this.roomsService.setupSocket();
@@ -80,9 +76,7 @@ export class RoomComponent implements OnInit, OnDestroy {
   }
 
   canExit() {
-    if (browserRefresh){
-      return confirm('Leaving the Room with delete all messages. Are you sure?')
-    }
+    console.log('canExit')
     return confirm('Leaving the Room with delete all messages. Are you sure?')
   }
 }
