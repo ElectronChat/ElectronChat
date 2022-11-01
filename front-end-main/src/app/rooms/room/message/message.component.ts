330b92ce0668111c4941538096e52788bf291bf0
TImmyTurner123 <55562474+TImmyTurner123@users.noreply.github.com>
2022-11-01 14:09:19 -0600

    Update message.component.ts

--- front-end-main/src/app/rooms/room/message/message.component.ts
+++ front-end-main/src/app/rooms/room/message/message.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core"
+import { Component, Input } from "@angular/core";
+
 @Component({
   selector: 'message',
   templateUrl: './message.component.html',
