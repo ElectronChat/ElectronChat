9de91da42fdad90f84b830a36f8f9f1568fcb7f9
TImmyTurner123 <55562474+TImmyTurner123@users.noreply.github.com>
2022-11-07 14:10:48 -0700

    Update Message.js

--- back-end-main/app/Models/Message.js
+++ back-end-main/app/Models/Message.js
@@ -1,13 +1,14 @@
 module.exports = class message
 {
-    constructor(name, message)
+    constructor(name, message,socketId)
     {
         this.name = name;
         this.message = message;
-
+        this.socketId = socketId;
     }
 
     getMessage() {return this.message};
     getName() {return this.name};
+    getSocket(){return this.socketId};
   
 }
