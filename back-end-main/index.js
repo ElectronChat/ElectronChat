f8bfd3035960a7403acdf8055e67d3a69cdfe0dd
James-R-Marshall <61856546+James-R-Marshall@users.noreply.github.com>
2022-12-05 13:08:09 -0700

    Update index.js

--- back-end-main/index.js
+++ back-end-main/index.js
@@ -10,7 +10,6 @@ app1.disable("x-powered-by");
 const home = require('./app/routes/main');
 const HandleIo = require("./app/Models/HandleIo.cjs");
 const server = http.createServer(app);
-const io = new Server(server);
 
 // this is our directory where all of our frontend gets built to.
 var distDir = __dirname + "/front/";
@@ -20,8 +19,6 @@ app.use(express.static('../front-end-main/dist'));
 // the first route is how we will start creating our routes.
 app.use('/', home);
 
-// socket.io handler
-var handler = new HandleIo(io)
 
 const hostname = process.env.host;
 const port = parseInt(process.env.port);
